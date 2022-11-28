7d538c2be787a5ea72e42066bd3014598b92afac
Murugan <anandhi.m@capgemini.com>
2022-11-28 17:18:42 +0530

    Ec2 Instance created

---
+++ main.tf
@@ -0,0 +1,19 @@
+# AWS Provider
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAV5H7H3DHVGPCYGWN"
+  secret_key = "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"
+}
+
+#AWS Instance
+
+resource "aws_instance" "Demo-instance" {
+     ami = ami-0d5bf08bc8017c83b
+     instance_type = "t2.micro"
+     availability_zone = var.availability_zone
+
+lifecycle {
+     ignore_changes = [ami]
+     }
+ }
